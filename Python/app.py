f8b911ef9243a3d483fb80a7fb7de8b95e8e4362
Gopika011 <gopikaa011@gmail.com>
2024-09-29 17:46:16 +0530

    inital commit

---
+++ Python/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from sqlalchemy import inspect
+from datetime import date
+from models import Movies,db
+
+
+app = Flask(__name__) #cretae instance of flask
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app) # Initialize the db with the app
+
+
+with app.app_context():
+    db.drop_all()   # This will drop all tables 
+    db.create_all() # This will recreate them- SQLAlchemy will look at the imported models and create tables
+
+    movie1 = Movies(title="Inception",genre="Sci-fi",release_date=date(2010,7,16), rating=8.8)
+    db.session.add(movie1)
+    db.session.commit()
+
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    print(f"Tables created: {tables}")
+
+    movies = Movies.query.all()
+    print(f"Movies in the database: {[movie.title for movie in movies]}")
+
+@app.route('/')
+def index():
+    return 'flask with sqlite'
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
---
+++ Python/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  # Initialize the SQLAlchemy object
+
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(100))
+    release_date = db.Column(db.Date)
+    rating = db.Column(db.Float)
