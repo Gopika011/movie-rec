cae2e1d6972497fa4c01bd267ab995ce8efb3e22
Gopika011 <gopikaa011@gmail.com>
2024-09-30 23:39:21 +0530

    basic authentication

--- Python/models.py
+++ Python/models.py
@@ -14,3 +14,14 @@ class Movies(db.Model):
     def __repr__(self):
         return f"movie with title {self.title}" 
     
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"user {self.username}" 
+    
